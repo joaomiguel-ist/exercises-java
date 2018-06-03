ff0a16703674a9deb1b2a3096498eb76435e268e
joaomiguel-ist <joaomdc.costa@gmail.com>
2018-06-04 00:59:15 +0100

    Create fizzbuzz.java

---
+++ fizzbuzz.java
@@ -0,0 +1,19 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i=1; i<201;i++){
+			
+			if((i%3 == 0) && (i%5 == 0)){
+				System.out.println("fizzbuzz");
+			}
+			else if(i%3 == 0){
+				System.out.println("fizz");
+			}
+			else if(i%5 == 0){
+				System.out.println("buzz");
+			}
+			else System.out.println(i);
+		}
+	}
+}
